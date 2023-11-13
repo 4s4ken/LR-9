d9f7f7bb3f169c2afb2caba5b865aa3bf5c27476
4skn <65232734+4s4ken@users.noreply.github.com>
2023-11-13 21:56:56 +0500

    Update LR-9.cpp
    Исправлены мелкие ошибки, улучшен диалог с пользователем.

--- LR-9/LR-9/LR-9.cpp
+++ LR-9/LR-9/LR-9.cpp
@@ -1,4 +1,4 @@
-﻿#include<iostream>
+#include<iostream>
 #include<Windows.h>
 #include <cstdio>
 #include <cstdlib>
@@ -13,12 +13,22 @@ int main() {
     string timeString = ctime(&currentTime);
     int N, randomNum;
     cout << "Введите значение N: " << endl;
+input: 
     cin >> N;
-    outputFile << N << " Время ввода значения N: " << timeString << endl;
+    if (!cin)
+    {
+        cin.clear();
+        cin.ignore(50, '\n');
+        cout << "Ошибка в вводе значения N, попробуйте снова." << endl;
+        outputFile << "Ошибка при ввода числа N - " << timeString << endl;
+        goto input;
+    }
+    outputFile << "Входные данные: " << endl << N << " Время ввода значения N: " << timeString << endl;
     int* A = new int[N];
     for (int i = 0;i < N;i++)
         A[i] = i;
     randomNum = rand() % N + 1;
+    outputFile << "Выходные данные:" << endl;
     for (int i = 1;i < N+1;i++)
     {
         randomNum = rand() % N + 1;;
@@ -26,7 +36,7 @@ int main() {
         if (A[randomNum] != 0)
         {
             getchar();
-            cout << randomNum;
+            cout << "Бочонок под номером " << i<< " имеет номер: " << randomNum;
             time_t currentTime = time(0);
             string timeString = ctime(&currentTime);
             outputFile << randomNum << " Время вывода " << i << "-го числа: " << timeString << endl;
